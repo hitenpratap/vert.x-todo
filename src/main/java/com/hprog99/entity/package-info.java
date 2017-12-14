d64ef4e048502cb2f524078c785636c49768a098
Hiten Pratap Singh <hitenpratap99@gmail.com>
2017-12-14 06:15:46 +0530

    Initial commit

---
+++ src/main/java/com/hprog99/entity/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Indicates that this module contains classes that need to be generated / processed.
+ */
+@ModuleGen(name = "vertx-blueprint-todo-entity", groupPackage = "com.hprog99.entity")
+package com.hprog99.entity;
+
+import io.vertx.codegen.annotations.ModuleGen;
